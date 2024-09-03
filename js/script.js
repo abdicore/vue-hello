4b0ffb3a854dfcab5e0f02e539e654ce4db46bb0
abdicore <abdirhamanmohamed@gmail.com>
2024-09-03 23:03:22 +0200

    img

--- js/script.js
+++ js/script.js
@@ -1,10 +1,3 @@
-/*
-Esercizio di oggi: Vue Hello
-Descrizione:
-Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
-Bonus:
-Aggiungere alla pagina un’immagine, presa anch’essa da un data.
-*/
 
 const { createApp } = Vue
 
